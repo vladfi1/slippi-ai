a6409e1cce39575da0929d7604c6ce0de4ba8695
Vlad Firoiu <vladfi2@gmail.com>
2020-11-14 12:52:07 -0500

    Load data from altf4's dataset.

--- learner.py
+++ learner.py
@@ -1,5 +1,6 @@
 """Learner test script."""
 
+import itertools
 import os
 
 from absl import app
@@ -11,20 +12,38 @@ import tensorflow as tf
 
 import melee
 import embed
+import stats
 
 REPLAY_PATH = os.path.expanduser('~/Slippi/Game_20201113T162336.slp')
+DATASET_PATH = '/media/vlad/Archive-2T-Nomad/Vlad/training_data/'
 
 def read_gamestates(replay_path):
+  print("Reading from ", replay_path)
   console = melee.Console(is_dolphin=False,
-                          allow_old_version=False,
+                          allow_old_version=True,
                           path=replay_path)
   console.connect()
 
-  while True:
-    gamestate = console.step()
-    if gamestate is None:
-      break
+  gamestate = console.step()
+  port_map = dict(zip(gamestate.player.keys(), [1, 2]))
+
+  def fix_state(s):
+    s.player = {port_map[p]: v for p, v in s.player.items()}
+
+  while gamestate:
+    fix_state(gamestate)
     yield gamestate
+    gamestate = console.step()
+
+class TrajectoryProducer:
+  def __init__(self, embed_game):
+    self.embed_game = embed_game
+
+  def __iter__(self):
+    for name in itertools.cycle(stats.table['filename']):
+      path = DATASET_PATH + name
+      yield [self.embed_game.from_state(s) for s in read_gamestates(path)]
+      # TODO: also yield with players swapped?
 
 def np_array(*vals):
   return np.array(vals)
@@ -41,6 +60,35 @@ def to_time_major(t):
   permutation[1] = 0
   return tf.transpose(t, permutation)
 
+class TrajectoryManager:
+  # TODO: manage recurrent state? can also do it in the learner
+
+  def __init__(self, source, num_frames: int):
+    self.source = source
+    self.num_frames = num_frames
+    self.frames = self.iter_frames()
+
+  def iter_frames(self):
+    for trajectory in self.source:
+      restart = True
+      for frame in trajectory:
+        yield frame, restart
+        restart = False
+
+  def next(self):
+    frames, restarts = zip(*[next(self.frames) for _ in range(self.num_frames)])
+    return batch_nest(frames), np.array(restarts)
+
+class DataSource:
+  def __init__(self, embed_game, batch_size=64, unroll_length=64):
+    producer = iter(TrajectoryProducer(embed_game))
+    self.managers = [
+        TrajectoryManager(producer, unroll_length)
+        for _ in range(batch_size)]
+
+  def __next__(self):
+    return batch_nest([m.next() for m in self.managers])
+
 class Learner:
 
   def __init__(self, embed_game):
@@ -48,7 +96,7 @@ class Learner:
     self.optimizer = snt.optimizers.Adam(1e-4)
     self.embed_game = embed_game
 
-  def step(self, batch_gamestate):
+  def step(self, batch_gamestate, restarting):
     time_major_gamestate = tf.nest.map_structure(to_time_major, batch_gamestate)
 
     flat_gamestate = self.embed_game(time_major_gamestate)
@@ -71,24 +119,12 @@ class Learner:
     return loss
 
 def main(_):
-  gamestates = list(read_gamestates(REPLAY_PATH))
-  print(len(gamestates))
-
   embed_game = embed.make_game_embedding()
-  trajectory = make_trajectory(embed_game, gamestates[:10])
-
-  trajectories = [trajectory] * 32
-  batch_trajectories = batch_nest(trajectories)
-
-  # batch_signature = tf.nest.map_structure(
-  #     lambda a: (a.dtype, a.shape),
-  #     batch_trajectories)
-  # print(batch_signature)
-
   learner = Learner(embed_game)
+  data_source = DataSource(embed_game)
 
   for _ in range(1000):
-    loss = learner.step(batch_trajectories)
+    loss = learner.step(*next(data_source))
     print(loss.numpy())
 
 if __name__ == '__main__':
