28bc46798c9b814c6b630361bdcfee92bbd893f3
Vlad Firoiu <vladfi2@gmail.com>
2020-11-21 14:55:39 -0500

    Nested dynamic_rnn with test.

--- utils.py
+++ utils.py
@@ -11,14 +11,49 @@ def batch_nest(nests):
   return tf.nest.map_structure(np_array, *nests)
 
 def dynamic_rnn(core, inputs, initial_state):
-  unroll_length = tf.shape(inputs)[0]
-  outputs = tf.TensorArray(dtype=tf.float32, size=unroll_length)
-  state = initial_state
-  for i in tf.range(unroll_length):
-    input_ = inputs[i]  # TODO: handle nested inputs
-    output, state = core(input_, state)
-    outputs = outputs.write(i, output)
-  return outputs.stack(), state
+  """Dynamically unrolls an rnn core.
+
+  In the simple case of flat inputs and outputs, executes:
+  T = len(inputs)
+  state[0] = initial_state
+  outputs[i], state[i+1] = core(inputs[i], state[i])
+  returns: outputs, state[T]
+
+  In general inputs and outputs can be nests of tensors.
+
+  Args:
+    core: A callable with signature (input, state) -> (output, state).
+      For example, a snt.LSTM.
+    inputs: A nest of time-major tensors.
+    initial_state: A nest of tensors.
+  Returns:
+    A tuple (outputs, final_state).
+  """
+  unroll_length = tf.shape(tf.nest.flatten(inputs)[0])[0]
+
+  def get_input(index):
+    return tf.nest.map_structure(lambda t: t[index], inputs)
+
+  output_0, state = core(get_input(0), initial_state)
+
+  outputs = tf.nest.map_structure(
+      lambda t: tf.TensorArray(
+          dtype=t.dtype, size=unroll_length, element_shape=t.shape),
+      output_0)
+
+  def write_output(index, output):
+    return tf.nest.map_structure(
+        lambda ta, t: ta.write(index, t),
+        outputs, output)
+
+  outputs = write_output(0, output_0)
+
+  for i in tf.range(1, unroll_length):
+    output, state = core(get_input(i), state)
+    outputs = write_output(i, output)
+
+  outputs = tf.nest.map_structure(lambda ta: ta.stack(), outputs)
+  return outputs, state
 
 class Profiler:
   def __init__(self):
