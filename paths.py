41b57a3cad15cb728e40ad4f214e3bd9dd0a6217
Vlad Firoiu <vladfi2@gmail.com>
2021-09-17 18:16:51 +0100

    multishine processed game

--- paths.py
+++ paths.py
@@ -5,5 +5,6 @@ DATA_PATH = PRJ_PATH + 'data/'
 
 DB_PATH = DATA_PATH + 'melee_public_slp_dataset.sqlite3'
 REPLAY_PATH = DATA_PATH + 'Game_20201113T162336.slp'
+MULTISHINE_PATH = DATA_PATH + 'multishine/multishine.slp.pkl'
 DATASET_PATH = DATA_PATH + 'training_data/'
 COMPRESSED_PATH = DATA_PATH + 'AllCompressed/'
