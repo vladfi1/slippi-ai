e1a83a5f52c0e6f67726384214ecfd944af9e6b3
huntermcnicholsmbp <hmcnich@gmail.com>
2022-01-20 15:51:23 -0500

    Adding relative position infomation to transformer

--- transformers.py
+++ transformers.py
@@ -1,6 +1,6 @@
 import sonnet as snt
 import tensorflow as tf
-import math
+from math import ceil
 import numpy as np
 
 def positional_encoding(seq_len, d_model, batch_size=1):
@@ -17,15 +17,17 @@ def positional_encoding(seq_len, d_model, batch_size=1):
         denom = tf.math.pow(10000., 2. * i/d)
         return pos / denom
 
-    i_tensor = tf.expand_dims(tf.range(0, d_model//2), 0) # [1, d_model/2]
-    i_tensor = tf.broadcast_to(i_tensor, [seq_len, d_model//2]) # [seq_len, d_model/2]
+    num_angles = ceil(d_model/2)
+    i_tensor = tf.expand_dims(tf.range(0, num_angles), 0) # [1, d_model/2]
+    i_tensor = tf.broadcast_to(i_tensor, [seq_len, num_angles]) # [seq_len, d_model/2]
     j_tensor = tf.expand_dims(tf.range(0, seq_len), 1) # [seq_len, 1]
-    j_tensor = tf.broadcast_to(j_tensor, [seq_len, d_model//2]) # [seq_len, d_model/2]
+    j_tensor = tf.broadcast_to(j_tensor, [seq_len, num_angles]) # [seq_len, d_model/2]
     angles = encoding_angle(j_tensor, i_tensor) # [seq_len, d_model/2]
 
     # Apply sin to even indices, cos to odd indices
+    num_cos = num_angles if num_angles == d_model//2 else d_model//2 # reminder goes to sin
     sins_angle = tf.math.sin(angles) # [seq_len, d_model/2]
-    coss_angles = tf.math.cos(angles) # [seq_len, d_model/2]
+    coss_angles = tf.math.cos(angles[:, 0:num_cos]) # [seq_len, d_model/2]
     joined = tf.concat([sins_angle, coss_angles], -1) # [s, d]
 
     #Add in batch
@@ -79,6 +81,65 @@ def attention(queries, keys, values, masked=True, mem_size=None):
     att = tf.matmul(probs, values) # [B, S, D_V]
     return att
 
+def attention_relative(queries, keys, values, relpos_k, relpos_v, masked=True, mem_size=None):
+    """
+    Returns the 'attention' between three sequences: keys, queries, and values
+    Specifically this implementation uses 'scaled dot-product' attention.
+
+    This can be seen as a measure of the compatibility or relative importance between the keys and queries.
+    This compatilbility is then applied to the 'input' sequence represented by values.
+
+    Returns a tensor with the same shape as Values where [b, i ,j] represents the relative importance
+    "attention" of element j in the sequence.
+
+    When masked is True, only the elements prior to current position in the sequence are considered for compatibility.
+
+    When mem_size is not None, only consider the mem_size prior elements for compatibility. Requires masked to be True
+
+    keys: (batch, seq_len, D_k)
+    queries: (batch, seq_len, D_k)
+    values: (batch, seq_len, D_v)
+    relpos_k: (seq, seq, D_k)
+    relpos_v: (seq, seq, D_v)
+    mem_size: default is None, when set to positive integer will only consider mem_size prior elements for computation
+    returns: (batch, seq_len, D_v)
+    """
+    if mem_size is not None:
+      # TODO change this assumption (low priority and probably not useful)
+      assert masked is True, "In order to use attention memory masked must be set to true"
+    tf.debugging.assert_shapes([
+        (keys, ('B', 'S', 'D_k')),
+        (queries, ('B', 'S', 'D_k')),
+        (values, ('B', 'S', 'D_v')),
+    ])
+    B, S, D_k = tf.unstack(tf.shape(keys))
+
+    # compat [b, i, j] is the dot product of key i and query j (for batch # b)
+    compat = tf.matmul(queries, keys, transpose_b=True) # [B, S, S]
+    # Create second term
+    queries_t = tf.transpose(queries, [1, 0, 2]) # [S, B, D_k]
+    relpos_k_t = tf.transpose(relpos_k, [0, 2, 1]) # [S, D_k, S] is this right perm?
+    q_relp_matmul = tf.matmul(queries_t, relpos_k_t) # [S, B, S]
+    q_relp_matmul_t = tf.transpose(q_relp_matmul, [1, 0, 2]) # [B, S, S]
+    adj_compat = compat + q_relp_matmul_t # [B, S, S]
+    norm_compat = adj_compat / tf.sqrt(tf.cast(D_k, adj_compat.dtype)) # [B, S, S]
+    if masked:
+      i = tf.expand_dims(tf.range(S), 1)  # [S, 1]
+      j = tf.expand_dims(tf.range(S), 0)  # [1, S]
+      mask = i >= j # [S, S], mask[i, j] == i >= j
+      if mem_size is not None:
+        b_mask = i <= (j + mem_size)
+        mask = tf.math.logical_and(b_mask, mask)
+      norm_compat = tf.where(mask, norm_compat, np.NINF)
+    probs = tf.nn.softmax(norm_compat) # [B, S, S]
+    att = tf.matmul(probs, values) # [B, S, D_V]
+    # Create second term
+    probs_t = tf.transpose(probs, [1, 0, 2]) # [S, B, S]
+    probs_relv_matmul = tf.matmul(probs_t, relpos_v) # [S, B, D_v]
+    probs_relv_matmul_t = tf.transpose(probs_relv_matmul, [1, 0, 2]) # [B, S, D_v]
+    adj_att = att + probs_relv_matmul_t
+    return adj_att
+
 class MultiHeadAttentionBlock(snt.Module):
   def __init__(self, num_heads, output_size,  mem_size=None, name='MultiHeadAttentionBlock'):
     super(MultiHeadAttentionBlock, self).__init__()
@@ -89,20 +150,34 @@ class MultiHeadAttentionBlock(snt.Module):
     self.W_V = []
     self.W_Q = []
     assert output_size%num_heads == 0, "output_size must be a multiple of num_heads"
-    projection_size = output_size//num_heads
+    self.projection_size = output_size//num_heads
     for _ in range(num_heads):
         # TODO there's a more efficient way to do this https://github.com/wmcnicho/slippi-ai/pull/2/commits/3354fcb94aeb2c2354352eb893fb454ed2dab9e3
-      self.W_K.append(snt.Linear(int(projection_size))) #output is d_model/num_heads
-      self.W_V.append(snt.Linear(int(projection_size))) #output is d_model/num_heads
-      self.W_Q.append(snt.Linear(int(projection_size))) #output is d_model/num_heads
+      self.W_K.append(snt.Linear(int(self.projection_size))) #output is d_model/num_heads
+      self.W_V.append(snt.Linear(int(self.projection_size))) #output is d_model/num_heads
+      self.W_Q.append(snt.Linear(int(self.projection_size))) #output is d_model/num_heads
     self.W_O = snt.Linear(output_size)
     self.l1_embed = snt.Linear(output_size)
+
+  @snt.once
+  def _initialize(self, inputs):
+    input_seq = inputs.shape[1]
+    S = input_seq + self.mem_size
+    # Shared across heads
+    self.relpos_k = tf.Variable(tf.zeros([S, S, self.projection_size]))
+    self.relpos_v = tf.Variable(tf.zeros([S, S, self.projection_size]))
 
   def initial_state(self, batch_size):
     if self.mem_size is None:
       raise NotImplementedError()
     else:
       return tf.zeros([batch_size, self.mem_size, self.output_size])
+
+  def build_relpos(self, shape):
+    size = shape[1] + self.mem_size
+    rk = self.relpos_k[:size, :size, :]
+    rv = self.relpos_v[:size, :size, :]
+    return (rk, rv)
 
   def __call__(self, inputs, prev_state):
     """
@@ -114,6 +189,7 @@ class MultiHeadAttentionBlock(snt.Module):
     inputs: [B, S, D_m]
     returns: (output: [B, S, D_m], next_state: [B, M, D_m])
     """
+    self._initialize(inputs)
     B, S, D_m = tf.unstack(tf.shape(inputs))
     M = self.mem_size
     if inputs.shape[-1] is not self.output_size:
@@ -125,16 +201,18 @@ class MultiHeadAttentionBlock(snt.Module):
     combined_input = tf.concat([prev_state, inputs], 1) # [B, M+S, D_m]
     for i in range(self.num_heads):
       # head_i <- Attention(QW_i^Q, KW_i^K, VW_i^V)
-      # TODO technically we don't need to use queries:  
-      # See https://github.com/vladfi1/slippi-ai/commit/bfaefd000279fb187f8e155c74a02f3a547e6154
       queries = self.W_Q[i](combined_input) # [B, M+S, D_m/h]
       keys = self.W_K[i](combined_input) # [B, M+S, D_m/h]
       values = self.W_V[i](combined_input) # [B, M+S, D_m/h]
-      head_i = attention(queries, keys, values, mem_size=M) # [B, M+S, D_m/h]
+      #head_i = attention(queries, keys, values, mem_size=M) # [B, M+S, D_m/h]
+      rk, rv = self.build_relpos(inputs.shape)
+      head_i = attention_relative(queries, keys, values, rk, rv, mem_size=M) # [B, M+S, D_m/h]
       heads.append(head_i)
     # MHA(Q, K, V) <- Concat(head_1...head_h)W^O
     proj_heads = self.W_O(tf.concat(heads, -1))  # [B, M+S, D_m]
     multi_head = proj_heads[:, M:] # [B, S, D_m]
+    # TODO technically we don't need to track queries in memory  
+    # See https://github.com/vladfi1/slippi-ai/commit/bfaefd000279fb187f8e155c74a02f3a547e6154
     next_state = combined_input[:,S:] # [B, M, D_m]
     return multi_head, next_state
 
@@ -187,14 +265,13 @@ class EncoderOnlyTransformer(snt.Module):
       prev_sate: [L, B, M, O]
     """
     inputs = self.shape_convert(inputs) # [T, B, O]
-    inputs = tf.transpose(inputs, [1, 0, 2]) # [B, T, O]
-    i_shape = tf.shape(inputs)
-    # HACKED FOR TESTING UNTIL RELATIVE ENCODING
+    x = tf.transpose(inputs, [1, 0, 2]) # [B, T, O]
+    # Disabled to favor relative encoding, this could be on flag
+    #i_shape = tf.shape(inputs)
     #encoding = positional_encoding(i_shape[1], i_shape[2], batch_size=i_shape[0])
-    #x = inputs + encoding # [B, T, O]
-    x = inputs
+    #x = x + encoding # [B, T, O]
     next_state = []
-    for t, p in zip(self.transformer_blocks, prev_state):
+    for t, p in zip(self.transformer_blocks,  prev_state):
       x,n = t(x, p) # [B, T, O]
       next_state.append(n)
     x = tf.transpose(x, [1, 0, 2]) # [T, B, O]
