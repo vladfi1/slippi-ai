2ed9a9470e714cd967cad2cbba9d38525049ae29
Vlad Firoiu <vladfi2@gmail.com>
2022-06-03 10:56:38 +0100

    Update run_dolphin script.

--- scripts/run_dolphin.py
+++ scripts/run_dolphin.py
@@ -2,23 +2,21 @@ import time
 
 from absl import app
 from absl import flags
+import fancyflags as ff
 
+from slippi_ai import eval_lib
 from slippi_ai import dolphin as dolphin_lib
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string('dolphin_path', None, 'Path to dolphin directory.', required=True)
-flags.DEFINE_string('iso_path', None, 'Path to SSBM iso.', required=True)
+DOLPHIN = ff.DEFINE_dict('dolphin', **eval_lib.DOLPHIN_FLAGS)
 flags.DEFINE_integer('N', 1, 'number of dolphin instances')
 flags.DEFINE_integer('frames', 1 * 60 * 60, 'number of frames to run for')
 flags.DEFINE_boolean('render', False, 'render graphics')
 
 def main(_):
-  players = {1: dolphin_lib.CPU(), 2: dolphin_lib.CPU()}
+  players = {1: dolphin_lib.AI(), 2: dolphin_lib.CPU()}
 
-  dolphin = dolphin_lib.Dolphin(
-      FLAGS.dolphin_path, FLAGS.iso_path, players,
-      render=FLAGS.render,
-  )
+  dolphin = dolphin_lib.Dolphin(players=players, **DOLPHIN.value)
 
   dolphin.step()
 
