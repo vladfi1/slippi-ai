3f3041708dc1d52c0f3667779d1de82cb8f1f4eb
Vlad Firoiu <vladfi2@gmail.com>
2022-07-11 17:17:24 +0000

    Run rllib's ppo algorithm with wandb on aws.

---
+++ scripts/launch_ppo.sh
@@ -0,0 +1,6 @@
+ray submit clusters/rllib.yaml slippi_ai/rllib/ppo.py \
+  --dolphin.path /install/squashfs-root/usr/bin/ \
+  --dolphin.iso /install/SSBM.iso \
+  --dolphin.headless True \
+  --ray_init --wandb.use True \
+  --config.num_workers 4 --env.num_envs 2
--- slippi_ai/rllib/env.py
+++ slippi_ai/rllib/env.py
@@ -12,8 +12,6 @@ from slippi_ai import eval_lib, embed, reward
 from slippi_ai import dolphin as dolphin_lib
 from slippi_db import parse_libmelee
 
-_ENV_ID = 0
-
 class AgentInfo(tp.NamedTuple):
   opponent_port: int
   embed_game: embed.Embedding = embed.default_embed_game
@@ -24,11 +22,14 @@ class MeleeEnv(rllib.BaseEnv):
   def __init__(
       self,
       dolphin_fn: tp.Callable[[], dolphin_lib.Dolphin],
+      num_envs: int = 1,
   ):
     self._dolphin_fn = dolphin_fn
-    self._dolphin = dolphin_fn()
+    self._num_envs = num_envs
 
-    players = self._dolphin._players
+    self._dolphins = [dolphin_fn() for _ in range(num_envs)]
+
+    players = self._dolphins[0]._players
     assert len(players) == 2
 
     self._agents: tp.Dict[int, AgentInfo] = {}
@@ -39,64 +40,75 @@ class MeleeEnv(rllib.BaseEnv):
       if isinstance(player, dolphin_lib.AI):
         self._agents[port] = AgentInfo(opponent_port)
 
-    self._prev_gamestate = None
+    self._prev_gamestates = None
 
-  def _step(self, num_retries=3):
+  def _step_one(self, index: int, num_retries=3):
     """Steps dolphin, possibly creating a new instance if we disconnect."""
     for _ in range(num_retries):
       try:
-        return self._dolphin.step()
+        return self._dolphins[index].step()
       except EnetDisconnected:
         logging.warn('EnetDisconnected, restarting dolphin.')
-        self._dolphin.stop()
-        self._dolphin = self._dolphin_fn()
-    return self._dolphin.step()
+        self._dolphins[index].stop()
+        from ray.util import pdb; pdb.set_trace()
+        self._dolphins[index] = self._dolphin_fn()
+    return self._dolphins[index].step()
+
+  def _step(self, num_retries=3):
+    return [self._step_one(i) for i in range(self._num_envs)]
 
   def poll(
       self,
   ) -> tp.Tuple[MultiEnvDict, MultiEnvDict, MultiEnvDict, MultiEnvDict, MultiEnvDict]:
-    if self._prev_gamestate is None:
-      self._prev_gamestate = self._step()
-    gamestate = self._step()
+    if self._prev_gamestates is None:
+      self._prev_gamestates = self._step()
 
-    observations = {}
-    rewards = {}
-    dones = {"__all__": False}
-    infos = {}
-    off_policy_actions = {}
+    all_envs_result = {}, {}, {}, {}, {}
 
-    for port, info in self._agents.items():
-      obs = parse_libmelee.get_game(gamestate, ports=(port, info.opponent_port))
-      obs = info.embed_game.from_state(obs)
-      obs = info.embed_game.to_nest(obs)
-      observations[port] = obs
+    for env_id in range(self._num_envs):
+      gamestate = self._step_one(env_id)
 
-      rewards[port] = reward.get_reward(
-          self._prev_gamestate, gamestate, port, info.opponent_port)
-      dones[port] = False  # TODO: check if we just menued
-      infos[port] = {}
+      observations = {}
+      rewards = {}
+      dones = {"__all__": False}
+      infos = {}
+      off_policy_actions = {}
 
-    self._prev_gamestate = gamestate
+      for port, info in self._agents.items():
+        obs = parse_libmelee.get_game(
+            gamestate, ports=(port, info.opponent_port))
+        obs = info.embed_game.from_state(obs)
+        obs = info.embed_game.to_nest(obs)
+        observations[port] = obs
 
-    result = observations, rewards, dones, infos, off_policy_actions
-    result = tuple({_ENV_ID: x} for x in result)
-    return result
+        rewards[port] = reward.get_reward(
+            self._prev_gamestates[env_id], gamestate, port, info.opponent_port)
+        dones[port] = False  # TODO: check if we just menued
+        infos[port] = {}
+
+      self._prev_gamestates[env_id] = gamestate
+
+      result = observations, rewards, dones, infos, off_policy_actions
+      for all_envs, value in zip(all_envs_result, result):
+        all_envs[env_id] = value
+
+    return all_envs_result
 
   def try_reset(self, env_id: EnvID = None) -> MultiEnvDict:
-    assert env_id in (None, _ENV_ID)
+    # assert env_id in (None, _ENV_ID)
     return self.poll()[0]
 
   def send_actions(self, action_dict: dict):
     # print('send_actions')
-    agent_actions = action_dict[_ENV_ID]
-    for port, controller in agent_actions.items():
-      embed_action = self._agents[port].embed_action
-      # convert from rllib Nests to our own NamedTuples
-      controller = embed_action.from_nest(controller)
-      # un-discretize sticks/shoulder
-      controller = embed_action.decode(controller)
-      eval_lib.send_controller(
-          self._dolphin.controllers[port], controller)
+    for env_id, agent_actions in action_dict.items():
+      for port, controller in agent_actions.items():
+        embed_action = self._agents[port].embed_action
+        # convert from rllib Nests to our own NamedTuples
+        controller = embed_action.from_nest(controller)
+        # un-discretize sticks/shoulder
+        controller = embed_action.decode(controller)
+        eval_lib.send_controller(
+            self._dolphins[env_id].controllers[port], controller)
 
   def get_agent_ids(self) -> tp.Set[AgentID]:
     return set(self._agents)
@@ -105,9 +117,9 @@ class MeleeEnv(rllib.BaseEnv):
       self, as_dict: bool = False,
   ) -> tp.Union[tp.List[EnvType], tp.Dict[EnvID, EnvType]]:
     if as_dict:
-      return {_ENV_ID: self}
+      return dict(enumerate(self._dolphins))
     else:
-      return [self]
+      return self._dolphins
 
   def stop(self):
     self._dolphin.stop()
@@ -124,7 +136,7 @@ class MeleeEnv(rllib.BaseEnv):
       sample = {a: self.observation_space[a].sample() for a in agent_id}
     else:
       sample = self.observation_space.sample()
-    return {_ENV_ID: sample}
+    return {env_id: sample for env_id in range(self._num_envs)}
 
   @functools.cached_property
   def action_space(self) -> gym.Space:
@@ -138,4 +150,4 @@ class MeleeEnv(rllib.BaseEnv):
       sample = {a: self.action_space[a].sample() for a in agent_id}
     else:
       sample = self.action_space.sample()
-    return {_ENV_ID: sample}
+    return {env_id: sample for env_id in range(self._num_envs)}
