a0b4d3345f1d25c1f6ac9de7cb15b615e92c083d
Vlad Firoiu <none@none>
2022-12-26 12:52:02 -0500

    Fix multishine script.

--- scripts/run_multishine.py
+++ scripts/run_multishine.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import argparse
 import signal
 import sys
 
 import melee
-from slippi_ai import techskill
+from slippi_ai import dolphin, techskill
 
 # This example program demonstrates how to use the Melee API to run a console,
 #   setup controllers, and send button presses over to a console
@@ -16,127 +16,59 @@ def check_port(value):
                                          Must be 1, 2, 3, or 4." % value)
     return ivalue
 
-parser = argparse.ArgumentParser(description='Example of libmelee in action')
-parser.add_argument('--debug', '-d', action='store_true',
-                    help='Debug mode. Creates a CSV of all game states')
-parser.add_argument('--address', '-a', default="127.0.0.1",
-                    help='IP address of Slippi/Wii')
-parser.add_argument('--dolphin_executable_path', '-e', default=None,
-                    help='The directory where dolphin is')
-parser.add_argument('--connect_code', '-t', default="",
-                    help='Direct connect code to connect to in Slippi Online')
-parser.add_argument('--iso', default=None, type=str,
-                    help='Path to melee iso.')
-parser.add_argument('--runtime', default=10, type=int,
-                    help='Runtime in seconds.')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Example of libmelee in action')
+    parser.add_argument('--debug', '-d', action='store_true',
+                        help='Debug mode. Creates a CSV of all game states')
+    parser.add_argument('--address', '-a', default="127.0.0.1",
+                        help='IP address of Slippi/Wii')
+    parser.add_argument('--dolphin_executable_path', '-e', default=None,
+                        help='The directory where dolphin is')
+    parser.add_argument('--connect_code', '-t', default="",
+                        help='Direct connect code to connect to in Slippi Online')
+    parser.add_argument('--iso', default=None, type=str,
+                        help='Path to melee iso.')
+    parser.add_argument('--runtime', default=10, type=int,
+                        help='Runtime in seconds.')
 
+    args = parser.parse_args()
 
-args = parser.parse_args()
-
-# This logger object is useful for retroactively debugging issues in your bot
-#   You can write things to it each frame, and it will create a CSV file describing the match
-log = None
-if args.debug:
-    log = melee.Logger()
-
-# Create our Console object.
-#   This will be one of the primary objects that we will interface with.
-#   The Console represents the virtual or hardware system Melee is playing on.
-#   Through this object, we can get "GameState" objects per-frame so that your
-#       bot can actually "see" what's happening in the game
-console = melee.Console(path=args.dolphin_executable_path,
-                        slippi_address=args.address,
-                        online_delay=0,
-                        logger=log,
-                        blocking_input=True)
-
-# Create our Controller object
-#   The controller is the second primary object your bot will interact with
-#   Your controller is your way of sending button presses to the game, whether
-#   virtual or physical.
-controllers = []
-agents = []
-
-for port in [1, 2]:
-    controller = melee.Controller(
-        console=console,
-        port=port,
-        type=melee.ControllerType.STANDARD)
-    controllers.append(controller)
-    agents.append(techskill.MultiShine(port, controller))
-
-# This isn't necessary, but makes it so that Dolphin will get killed when you ^C
-def signal_handler(sig, frame):
-    console.stop()
+    # This logger object is useful for retroactively debugging issues in your bot
+    #   You can write things to it each frame, and it will create a CSV file describing the match
+    log = None
     if args.debug:
-        log.writelog()
-        print("") #because the ^C will be on the terminal
-        print("Log file created: " + log.filename)
-    print("Shutting down cleanly...")
-    sys.exit(0)
+        log = melee.Logger()
 
-signal.signal(signal.SIGINT, signal_handler)
+    players = {
+        port: dolphin.AI(melee.Character.FOX)
+        for port in (1, 2)
+    }
 
-# Run the console
-console.run(iso_path=args.iso)
+    console = dolphin.Dolphin(
+        path=args.dolphin_executable_path,
+        iso=args.iso,
+        players=players,
+    )
 
-# Connect to the console
-print("Connecting to console...")
-if not console.connect():
-    print("ERROR: Failed to connect to the console.")
-    sys.exit(-1)
-print("Console connected")
+    # Create our Controller object
+    #   The controller is the second primary object your bot will interact with
+    #   Your controller is your way of sending button presses to the game, whether
+    #   virtual or physical.
+    agents = []
 
-# Plug our controller in
-#   Due to how named pipes work, this has to come AFTER running dolphin
-#   NOTE: If you're loading a movie file, don't connect the controller,
-#   dolphin will hang waiting for input and never receive it
-print("Connecting controller to console...")
-for c in controllers:
-    if not c.connect():
-        print("ERROR: Failed to connect the controller.")
-        sys.exit(-1)
-    print("Controller connected")
+    for port in [1, 2]:
+        agents.append(techskill.MultiShine(
+            port, console.controllers[port]))
 
-costume = 0
-framedata = melee.framedata.FrameData()
-num_frames = 0
+    costume = 0
+    framedata = melee.framedata.FrameData()
+    num_frames = 0
 
-# Main loop
-while num_frames < args.runtime * 60:
-    # "step" to the next frame
-    gamestate = console.step()
-    if gamestate is None:
-        continue
-
-    # The console object keeps track of how long your bot is taking to process frames
-    #   And can warn you if it's taking too long
-    # if console.processingtime * 1000 > 12:
-    #     print("WARNING: Last frame took " + str(console.processingtime*1000) + "ms to process.")
-
-    # What menu are we in?
-    if gamestate.menu_state in [melee.Menu.IN_GAME, melee.Menu.SUDDEN_DEATH]:
+    # Main loop
+    while num_frames < args.runtime * 60:
+        # "step" to the next frame
+        gamestate = console.step()
         for agent in agents:
             agent.step(gamestate)
-        num_frames += 1
 
-        # Log this frame's detailed info if we're in game
-        if log:
-            log.logframe(gamestate)
-            log.writeframe()
-    else:
-        for i, controller in enumerate(controllers):
-            melee.MenuHelper.menu_helper_simple(gamestate,
-                                                controller,
-                                                melee.Character.FOX,
-                                                melee.Stage.YOSHIS_STORY,
-                                                args.connect_code,
-                                                costume=i,
-                                                autostart=i == 0,
-                                                swag=False)
-
-        # If we're not in game, don't log the frame
-        if log:
-            log.skipframe()
-
-console.stop()
+    console.stop()
