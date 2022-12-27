1aa18b55d87aa0065b00242dd9e5ea4341b845ee
Vlad Firoiu <none@none>
2022-12-27 18:54:06 -0500

    Script to pull pickled models.

---
+++ scripts/pull_pickled_model.py
@@ -0,0 +1,23 @@
+import os
+
+from absl import app, flags
+
+from slippi_ai import s3_lib
+
+FLAGS = flags.FLAGS
+flags.DEFINE_string('tag', None, 'experiment tag', required=True)
+flags.DEFINE_string('s3_creds', None, 's3 credentials')
+flags.DEFINE_string('path', 'pickled_models/', 'path to put model')
+
+def main(_):
+  store = s3_lib.get_store()
+  keys = s3_lib.get_keys(FLAGS.tag)
+  combined_bytes = store.get(keys.combined)
+
+  path = os.path.join(FLAGS.path, FLAGS.tag)
+
+  with open(path, 'wb') as f:
+    f.write(combined_bytes)
+
+if __name__ == '__main__':
+  app.run(main)
