fe3b24eb26d77dcdcfb2a9acef638d79258369c1
Kyle Darling <kdarling95@yahoo.com>
2024-05-25 19:47:22 -0400

    Revised wrapper to install everytime. Small bug tweaks, rl directory now installable despite being a separate source.

--- Dockerfile
+++ Dockerfile
@@ -11,15 +11,24 @@ RUN conda create -n slippiai python=3.10 pip && conda clean -a
 ENV CONDA_DEFAULT_ENV=slippiai
 ENV PATH /opt/conda/envs/$CONDA_DEFAULT_ENV/bin:$PATH
 
-RUN echo "source activate slippiai" > ~/.bashrc
+RUN echo "source activate slippiai" > /etc/skel/.bashrc
 
 RUN /bin/bash -c "source activate slippiai && conda install -c conda-forge cudatoolkit=11.8.0 cudnn=8.9 -y"
 
+RUN apt-get update && apt-get install -y sudo && \
+    useradd -m -s /bin/bash slippiuser && \
+    echo 'slippiuser ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+
+RUN echo "source /opt/conda/bin/activate slippiai" >> /home/slippiuser/.bashrc
+
 COPY . /install
+RUN chown -R slippiuser:slippiuser /install
+
+USER slippiuser
 
 RUN cd /install && \
-    apt update && \
-    apt install -y p7zip-full rsync && \
+    sudo apt-get update && \
+    sudo apt-get install -y p7zip-full rsync && \
     pip install ray[default] -r requirements.txt peppi-py && \
     pip install .
 
--- scripts/docker_wrapper.sh
+++ scripts/docker_wrapper.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
 show_help() {
-  echo "Usage: $0 [-f|--force] <script> [arguments]"
+  echo "Usage: $0 [--rebuild] [--clear-hash] <script> [arguments]"
   echo
   echo "Arguments:"
   echo "  <script>      The script to run inside the Docker container."
   echo "  [arguments]   Arguments to pass to the script."
   echo
   echo "Options:"
-  echo "  -f, --force   Force rebuild the Docker image."
+  echo "  --rebuild     Force rebuild the Docker image."
   echo
   echo "Example:"
   echo "  $0 python /mnt/slippi-ai/scripts/run_evaluator.py"
 }
 
 FORCE_REBUILD=false
+CLEAR_HASH=false
 
 # Parse options and arguments
-while [[ "$1" == "-"* ]]; do
+while [[ "$1" == "--"* ]]; do
   case "$1" in
-    -f|--force)
+    --rebuild)
       FORCE_REBUILD=true
       shift
       ;;
-    -*)
+    --*)
       show_help
       exit 1
       ;;
   esac
 done
 
-if [ $# -lt 1 ]; then
+if [ $# -lt 1 ] && [ "$FORCE_REBUILD" = false ]; then
   show_help
   exit 1
 fi
@@ -39,43 +40,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(dirname "$SCRIPT_DIR")"
 
 cd "$ROOT_DIR" || { echo "Failed to change directory to $ROOT_DIR"; exit 1; }
-
-HASH_FILE="$SCRIPT_DIR/.file_hashes.txt"
-
-get_check_files() {
-  local ignore_file=".dockerignore"
-  local check_files=()
-
-  if [ -f $ignore_file ]; then
-    while IFS= read -r line; do
-      [[ -z "$line" || "$line" =~ ^# ]] && continue
-      if [[ "$line" =~ ^! ]]; then
-        check_files+=("${line:1}")
-      fi
-    done < $ignore_file
-  fi
-
-  echo "${check_files[@]}"
-}
-
-calculate_hashes() {
-  local files=("$@")
-  for file in "${files[@]}"; do
-    if [ -e "$file" ]; then
-      find "$file" -type f -exec sha256sum {} + | sort -k 2 | sha256sum
-    fi
-  done
-}
-
-CHECK_FILES=($(get_check_files))
-
-if [ -f $HASH_FILE ]; then
-  OLD_HASHES=$(cat $HASH_FILE)
-else
-  OLD_HASHES=""
-fi
-
-CURRENT_HASHES=$(calculate_hashes "${CHECK_FILES[@]}")
 
 if [ "$FORCE_REBUILD" = true ] || ! docker image inspect "slippi-ai:latest" > /dev/null 2>&1; then
   echo "Rebuilding the Docker image..."
@@ -86,12 +50,7 @@ if [ "$FORCE_REBUILD" = true ] || ! docker image inspect "slippi-ai:latest" > /d
   fi
 fi
 
-if [ "$OLD_HASHES" != "$CURRENT_HASHES" ]; then
-  echo "$CURRENT_HASHES" > $HASH_FILE
-  SCRIPT_COMMAND="pip install . && $@"
-else
-  SCRIPT_COMMAND="$@"
-fi
+SCRIPT_COMMAND="pip install . && $@"
 
 docker run --gpus all --rm --name slippi-ai -v "$ROOT_DIR":/mnt/slippi-ai slippi-ai:latest bash -c "$SCRIPT_COMMAND"
 if [ $? -ne 0 ]; then
