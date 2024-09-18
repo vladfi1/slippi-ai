81f06b1586845abd6df93953a3e6b89f9bb8cd0a
Vlad Firoiu <vladfi2@gmail.com>
2024-09-18 10:00:17 -0400

    [twitchbot] Fix stream choppiness by increasing fps to 60.

--- scripts/stream.sh
+++ scripts/stream.sh
@@ -7,9 +7,9 @@ TWITCH_STREAM=rtmp://live.twitch.tv/app/$TWITCH_KEY
 # TODO: restart command every 2 days to bypass Twitch stream cap
 
 ffmpeg -report \
-  -f x11grab -i :99 \
+  -framerate 60 -f x11grab -i :99 \
   -f alsa -ac 2 -i default \
-  -c:v h264_nvenc -g 60 -preset fast \
+  -c:v h264_nvenc -g 120 -preset fast \
   -b:v 3000k -maxrate 3000k -bufsize 6000k \
   -c:a aac -ar 44100 \
   -f flv \
