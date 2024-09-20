d387f4af85ac27a8d3f3d5d3da7fc4ea59de49ec
Vlad Firoiu <vladfi2@gmail.com>
2024-09-20 14:52:15 +0100

    Evaluate different agents against each other.

--- scripts/run_evaluator.py
+++ scripts/run_evaluator.py
@@ -12,7 +12,10 @@ if __name__ == '__main__':
 
   from slippi_ai import eval_lib, dolphin, utils, evaluators, flag_utils
 
-  default_dolphin_config = dolphin.DolphinConfig(infinite_time=False)
+  default_dolphin_config = dolphin.DolphinConfig(
+      infinite_time=False,
+      headless=True,
+  )
   DOLPHIN = ff.DEFINE_dict(
       'dolphin', **flag_utils.get_flags_from_default(default_dolphin_config))
 
@@ -27,32 +30,48 @@ if __name__ == '__main__':
   INNER_BATCH_SIZE = flags.DEFINE_integer(
       'inner_batch_size', 1, 'Number of environments to run sequentially.')
 
-  SELF_PLAY = flags.DEFINE_boolean('self_play', False, 'Self play.')
   USE_GPU = flags.DEFINE_boolean('use_gpu', False, 'Use GPU for inference.')
   NUM_AGENT_STEPS = flags.DEFINE_integer(
       'num_agent_steps', 0, 'Number of agent steps to batch.')
   AGENT = ff.DEFINE_dict('agent', **eval_lib.AGENT_FLAGS)
 
+  SELF_PLAY = flags.DEFINE_boolean('self_play', False, 'Self play.')
+  OPPONENT = ff.DEFINE_dict('opponent', **eval_lib.PLAYER_FLAGS)
+
   NUM_WORKERS = flags.DEFINE_integer('num_workers', 0, 'Number of rollout workers.')
 
   def main(_):
-
-    players = {
-        1: dolphin.AI(),
-        2: dolphin.AI() if SELF_PLAY.value else dolphin.CPU(),
-    }
-
-    dolphin_kwargs = dolphin.DolphinConfig.kwargs_from_flags(DOLPHIN.value)
-    dolphin_kwargs.update(players=players)
-
-    agent_kwargs: dict = AGENT.value.copy()
+    p1_agent_kwargs: dict = AGENT.value.copy()
     state = eval_lib.load_state(
-        path=agent_kwargs.pop('path'),
-        tag=agent_kwargs.pop('tag'))
-    agent_kwargs.update(
+        path=p1_agent_kwargs.pop('path'),
+        tag=p1_agent_kwargs.pop('tag'))
+    p1_agent_kwargs.update(
         state=state,
         batch_steps=NUM_AGENT_STEPS.value,
     )
+
+    agent_kwargs = {1: p1_agent_kwargs}
+
+    if SELF_PLAY.value:
+      p2 = dolphin.AI()
+      agent_kwargs[2] = p1_agent_kwargs
+    else:
+      p2 = eval_lib.get_player(**OPPONENT.value)
+
+      if isinstance(p2, dolphin.AI):
+        p2_agent_kwargs: dict = OPPONENT.value['ai']
+        p2_state = eval_lib.load_state(
+            path=p2_agent_kwargs.pop('path'),
+            tag=p2_agent_kwargs.pop('tag'))
+        p2_agent_kwargs.update(
+            state=p2_state,
+            batch_steps=NUM_AGENT_STEPS.value,
+        )
+        agent_kwargs[2] = p2_agent_kwargs
+
+    players = {1: dolphin.AI(), 2: p2}
+    dolphin_kwargs = dolphin.DolphinConfig.kwargs_from_flags(DOLPHIN.value)
+    dolphin_kwargs.update(players=players)
 
     env_kwargs = {}
     if ASYNC_ENVS.value:
@@ -62,9 +81,7 @@ if __name__ == '__main__':
       )
 
     evaluator_kwargs = dict(
-        agent_kwargs={
-            port: agent_kwargs for port, player in players.items()
-            if isinstance(player, dolphin.AI)},
+        agent_kwargs=agent_kwargs,
         dolphin_kwargs=dolphin_kwargs,
         num_envs=NUM_ENVS.value,
         async_envs=ASYNC_ENVS.value,
