a42f30a5d904f0f27fa8f1798480e314bc9f0a29
Vlad Firoiu <vladfi2@gmail.com>
2024-09-28 10:37:57 -0400

    [twitchbot] Don't make huge ffmpeg log files.

--- scripts/stream.sh
+++ scripts/stream.sh
@@ -6,7 +6,7 @@ TWITCH_STREAM=rtmp://live.twitch.tv/app/$TWITCH_KEY
 # TODO: for some reason the twitch stream is often choppy
 # TODO: restart command every 2 days to bypass Twitch stream cap
 
-ffmpeg -report \
+ffmpeg \
   -framerate 60 -f x11grab -i :99 \
   -f alsa -ac 2 -i default \
   -c:v h264_nvenc -g 120 -preset fast \
