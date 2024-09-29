f3def9fa1b1c8ce5cda7cf94cee97b24c62c02e5
Vlad Firoiu <vladfi2@gmail.com>
2024-09-29 20:21:07 +0100

    Improve test case.

--- tests/bug.py
+++ tests/bug.py
@@ -2,6 +2,17 @@ import numpy as np
 import tensorflow as tf
 import sonnet as snt
 
+class Thunk:
+
+  def __init__(self, constructor):
+    self.constructor = constructor
+    self.value = None
+
+  def get(self):
+    if self.value is None:
+      self.value = self.constructor()
+    return self.value
+
 def main():
   # Bug only occurs with check_numerics.
   tf.debugging.enable_check_numerics()
@@ -9,7 +20,7 @@ def main():
   batch_size = 3
   core_size = 5
   core = snt.GRU(core_size)
-  linear = snt.Linear(1)
+  v = Thunk(lambda: tf.Variable(1, dtype=tf.float32))
 
   @tf.function
   def compute(xs):
@@ -26,13 +37,20 @@ def main():
       # The actual error is attributed to the gradient computation.
       tape.gradient(outputs, tape.watched_variables())
 
+    # Error goes away if we initialize the variable outside the control-dep.
+    # v.get()
+
     # Control dependency on inputs is required.
     with tf.control_dependencies([xs]):
       # A variable must be created in this scope.
-      linear(xs)
+      xs + v.get()
 
   xs = np.zeros([2, batch_size, 1], dtype=np.float32)
 
   compute(xs)
 
 main()
+
+# The resulting error is something like this:
+# Inputs to operation cond/else/_1/cond/StatefulPartitionedCall/gradient_tape/scan/while/scan/while_grad/body/_372/gradient_tape/scan/while/gradients/AddN_6 of type AddN must have the same size and shape.  Input 0: [0] != input 1: [5,15]
+# 	 [[{{node gradient_tape/scan/while/gradients/AddN_6}}]] [Op:__inference_fn_with_cond_1472]
