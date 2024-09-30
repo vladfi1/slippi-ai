416c2fee520c5444a3fc8a3479e11768cf04a09d
Vlad Firoiu <vladfi2@gmail.com>
2024-09-30 11:03:49 +0100

    Fix batching issue in q_learner.

--- tests/train_q.sh
+++ tests/train_q.sh
@@ -7,10 +7,12 @@ python scripts/train_q.py \
   --config.dataset.meta_path=$DATASET/meta.json \
   --config.dataset.test_ratio=0.5 \
   --config.data.compressed=True \
+  --config.data.batch_size=2 \
+  --config.data.unroll_length=2 \
   --config.runtime.log_interval=4 \
   --config.runtime.max_runtime=10 \
   --config.runtime.eval_every_n=2 \
   --config.runtime.num_eval_steps=1 \
-  --config.network.mlp.depth=1 \
-  --config.network.mlp.width=1 \
+  --config.q_function.network.name=gru \
+  --config.q_function.network.gru.hidden_size=3 \
   "$@"
