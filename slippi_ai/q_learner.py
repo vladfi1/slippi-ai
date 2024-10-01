a070c5ebbecde920569895fae617d0b3b7e7e5a3
Vlad Firoiu <vladfi2@gmail.com>
2024-10-01 09:40:28 -0400

    Reduce memory usage by separate compilation in q-learning.

--- slippi_ai/q_learner.py
+++ slippi_ai/q_learner.py
@@ -1,13 +1,15 @@
 import dataclasses
+import typing as tp
 from typing import Optional
 
+import numpy as np
 import sonnet as snt
 import tensorflow as tf
 
 from slippi_ai.data import Batch, Frames
 from slippi_ai.policies import Policy, RecurrentState
 from slippi_ai import q_function as q_lib
-from slippi_ai import tf_utils
+from slippi_ai import tf_utils, embed
 
 def swap_axes(t, axis1=0, axis2=1):
   permutation = list(range(len(t.shape)))
@@ -24,6 +26,14 @@ class LearnerConfig:
 
   num_samples: int = 1
   q_policy_imitation_weight: float = 0
+
+# TODO: use tf.tile instead?
+replicate = lambda n: lambda t: tf.stack([t] * n)
+
+T = tp.TypeVar('T')
+
+def identity(x: T) -> T:
+  return x
 
 # TODO: should this be a snt.Module?
 class Learner:
@@ -48,9 +58,14 @@ class Learner:
     self.discount = 0.5 ** (1 / (reward_halflife * 60))
 
     if compile:
-      self.compiled_step = tf.function(self.step, jit_compile=jit_compile)
+      maybe_compile = tf.function(jit_compile=jit_compile, autograph=False)
     else:
-      self.compiled_step = self.step
+      maybe_compile = identity
+
+    self.train_sample_policy = maybe_compile(self._train_sample_policy)
+    self.train_q_function = maybe_compile(self._train_q_function)
+    self.train_q_policy = maybe_compile(self._train_q_policy)
+    self.compiled_step = self.step  # This isn't entirely accurate.
 
     self.num_samples = num_samples
     self.q_policy_imitation_weight = q_policy_imitation_weight
@@ -75,55 +90,34 @@ class Learner:
     self.q_function.initialize_variables()
     self.q_function_optimizer._initialize(self.q_function.trainable_variables)
 
-  def step(
+  def _train_sample_policy(
       self,
-      batch: Batch,
+      frames: Frames,
+      # batch: Batch,
       initial_states: RecurrentState,
       train: bool = True,
-  ):
-    bm_frames = batch.frames
-    restarting = batch.needs_reset
-    batch_size = restarting.shape[0]
+  ) -> tuple[embed.Action, RecurrentState, dict]:
+    # frames = batch.frames
+    # restarting = batch.needs_reset
+    # batch_size = restarting.shape[0]
 
-    # reset initial_states where necessary
-    restarting = tf.expand_dims(restarting, -1)
-    initial_states = tf.nest.map_structure(
-        lambda x, y: tf.where(restarting, x, y),
-        self.initial_state(batch_size),
-        initial_states)
+    # # reset initial_states where necessary
+    # restarting = tf.expand_dims(restarting, -1)
+    # initial_states = tf.nest.map_structure(
+    #     lambda x, y: tf.where(restarting, x, y),
+    #     self.sample_policy.initial_state(batch_size),
+    #     initial_states)
 
-    # switch axes to time-major
-    tm_frames: Frames = tf.nest.map_structure(
-        swap_axes, bm_frames)
-
-    # TODO: take into account delay
-
-    action = tm_frames.state_action.action
+    action = frames.state_action.action
     prev_action = tf.nest.map_structure(lambda t: t[:-1], action)
     next_action = tf.nest.map_structure(lambda t: t[1:], action)
-
-    to_train: list[tuple[list[tf.Tensor], list[tf.Variable], snt.Optimizer]] = []
 
     # Train sample policy with imitation
     with tf.GradientTape() as tape:
       sample_policy_outputs = self.sample_policy.unroll_with_outputs(
-          tm_frames, initial_states['sample_policy'])
+          frames, initial_states)
       sample_policy_loss = sample_policy_outputs.imitation_loss
 
-      sample_policy_vars = self.sample_policy.trainable_variables
-      tf_utils.assert_same_variables(tape.watched_variables(), sample_policy_vars)
-      # self.sample_policy_optimizer._initialize(sample_policy_vars)
-
-      if train:
-        sample_policy_grads = tape.gradient(sample_policy_loss, sample_policy_vars)
-        to_train.append((
-            sample_policy_grads,
-            sample_policy_vars,
-            self.sample_policy_optimizer,
-        ))
-
-    # TODO: use tf.tile instead?
-    replicate = lambda n: lambda t: tf.stack([t] * n)
     replicate_samples = replicate(self.num_samples)
 
     # Because the action space is too large, we compute a finite subsample
@@ -139,31 +133,56 @@ class Learner:
         policy_samples.controller_state, next_action,
     )
 
-    num_samples = self.num_samples + 1
-    replicate_samples = lambda nest: tf.nest.map_structure(
-        replicate(num_samples), nest)
+    # Defer updating the policy until after we sample from it.
+    if train:
+      sample_policy_vars = self.sample_policy.trainable_variables
+      tf_utils.assert_same_variables(tape.watched_variables(), sample_policy_vars)
+      sample_policy_grads = tape.gradient(sample_policy_loss, sample_policy_vars)
+      self.sample_policy_optimizer.apply(sample_policy_grads, sample_policy_vars)
 
-    # At this point tensorflow should be able to GC the large
-    # sample_policy_outputs.outputs tensor. We enforce a control dependency
-    # to help tensorflow figure out that it can do this.
+    return (
+        policy_samples,
+        sample_policy_outputs.final_state,
+        sample_policy_outputs.metrics
+    )
+
+  def _train_q_function(
+      self,
+      # batch: Batch,
+      frames: Frames,
+      initial_states: RecurrentState,
+      policy_samples: embed.Action,
+      train: bool = True,
+  ) -> tuple[tf.Tensor, RecurrentState, dict]:
+    # frames = batch.frames
+    # restarting = batch.needs_reset
+    # batch_size = restarting.shape[0]
+
+    # # reset initial_states where necessary
+    # restarting = tf.expand_dims(restarting, -1)
+    # initial_states = tf.nest.map_structure(
+    #     lambda x, y: tf.where(restarting, x, y),
+    #     self.q_function.initial_state(batch_size),
+    #     initial_states)
 
     # Train q function with regression
-    with tf.control_dependencies([policy_samples]), tf.GradientTape() as tape:
+    with tf.GradientTape() as tape:
       # TODO: take into account delay
       q_outputs, q_final_states = self.q_function.loss(
-          tm_frames, initial_states['q_function'], self.discount)
+          frames, initial_states, self.discount)
 
-      q_function_vars = self.q_function.trainable_variables
-      tf_utils.assert_same_variables(tape.watched_variables(), q_function_vars)
-      # self.q_function_optimizer._initialize(q_function_vars)
+    optimize = True  # Don't recompute q-values of taken actions
 
-      if train:
-        q_function_grads = tape.gradient(q_outputs.loss, q_function_vars)
-        to_train.append((
-            q_function_grads,
-            q_function_vars,
-            self.q_function_optimizer,
-        ))
+    if optimize:
+      num_samples = self.num_samples
+      policy_samples = tf.nest.map_structure(
+          lambda t: t[:-1], policy_samples,
+      )
+    else:
+      num_samples = self.num_samples + 1
+
+    replicate_samples = lambda nest: tf.nest.map_structure(
+        replicate(num_samples), nest)
 
     # Compute the q-values of the sampled actions
     replicated_hidden_states = replicate_samples(q_outputs.hidden_states)
@@ -173,12 +192,51 @@ class Learner:
             actions=policy_samples,
         )
 
-    # At this point q_outputs.hidden_states can be GCed.
+    if optimize:
+      sample_q_values = tf.concat(
+          [sample_q_values, tf.expand_dims(q_outputs.q_values, 0)], axis=0)
+
+    # Defer updating q_function until after we apply it to the policy_samples
+    if train:
+      q_function_vars = self.q_function.trainable_variables
+      tf_utils.assert_same_variables(tape.watched_variables(), q_function_vars)
+      q_function_grads = tape.gradient(q_outputs.loss, q_function_vars)
+      self.q_function_optimizer.apply(q_function_grads, q_function_vars)
+
+    return sample_q_values, q_final_states, q_outputs.metrics
+
+  def _train_q_policy(
+      self,
+      frames: Frames,
+      # batch: Batch,
+      initial_states: RecurrentState,
+      policy_samples: embed.Action,
+      sample_q_values: tf.Tensor,
+      train: bool = True,
+  ) -> tuple[RecurrentState, dict]:
+    # frames = batch.frames
+    # restarting = batch.needs_reset
+    # batch_size = restarting.shape[0]
+
+    # # reset initial_states where necessary
+    # restarting = tf.expand_dims(restarting, -1)
+    # initial_states = tf.nest.map_structure(
+    #     lambda x, y: tf.where(restarting, x, y),
+    #     self.q_policy.initial_state(batch_size),
+    #     initial_states)
+
+    action = frames.state_action.action
+    prev_action = tf.nest.map_structure(lambda t: t[:-1], action)
+
+    num_samples = self.num_samples + 1
+    assert sample_q_values.shape.as_list()[0] == num_samples
+    replicate_samples = lambda nest: tf.nest.map_structure(
+        replicate(num_samples), nest)
 
     # Train the q_policy by argmaxing the q_function over the sample_policy
-    with tf.control_dependencies([sample_q_values]), tf.GradientTape() as tape:
+    with tf.GradientTape() as tape:
       q_policy_outputs = self.q_policy.unroll_with_outputs(
-          tm_frames, initial_states['q_policy'])
+          frames, initial_states)
 
       # Construct a target distribution over the subsample and regress the
       # q_policy to this target.
@@ -205,36 +263,78 @@ class Learner:
       q_policy_total_loss = q_policy_q_loss
       q_policy_total_loss += self.q_policy_imitation_weight * q_policy_imitation_loss
 
-      q_policy_metrics = dict(
-          q_loss=q_policy_q_loss,
-          imitation_loss=q_policy_imitation_loss,
-      )
+    q_policy_metrics = dict(
+        q_loss=q_policy_q_loss,
+        imitation_loss=q_policy_imitation_loss,
+    )
 
+    if train:
       q_policy_vars = self.q_policy.trainable_variables
       q_policy_grads = tape.gradient(q_policy_total_loss, q_policy_vars)
-      # self.q_policy_optimizer._initialize(q_policy_vars)
+      self.q_policy_optimizer.apply(q_policy_grads, q_policy_vars)
 
-      if train:
-        to_train.append((
-            q_policy_grads,
-            q_policy_vars,
-            self.q_policy_optimizer,
-        ))
+    return q_policy_outputs.final_state, q_policy_metrics
 
-    for grads, vars, optimizer in to_train:
-      optimizer.apply(grads, vars)
+  # @tf.function(jit_compile=True)
+  def reset_initial_states(
+      self,
+      initial_states: RecurrentState,
+      restarting: np.ndarray,
+  ) -> RecurrentState:
+    batch_size = restarting.shape[0]
+    restarting = tf.expand_dims(restarting, -1)
+    return tf.nest.map_structure(
+        lambda x, y: tf.where(restarting, x, y),
+        self.initial_state(batch_size),
+        initial_states)
 
-    final_states = dict(
-        q_function=q_final_states,
-        q_policy=q_policy_outputs.final_state,
-        sample_policy=sample_policy_outputs.final_state,
-    )
+  def step(
+      self,
+      batch: Batch,
+      initial_states: RecurrentState,
+      train: bool = True,
+  ) -> tuple[dict, RecurrentState]:
+    # switch axes to time-major
+    tm_frames: Frames = tf.nest.map_structure(
+        lambda a: np.swapaxes(a, 0, 1), batch.frames)
+    # Put on device memory once.
+    tm_frames: Frames = tf.nest.map_structure(tf.convert_to_tensor, tm_frames)
 
-    metrics = dict(
-        total_loss=q_policy_q_loss,  # satisfy train_q_lib._get_loss
-        q_function=q_outputs.metrics,
-        q_policy=q_policy_metrics,
-        sample_policy=sample_policy_outputs.metrics,
-    )
+    # tm_batch = batch._replace(frames=tm_frames)
+    # tm_batch: Batch = tf.nest.map_structure(tf.convert_to_tensor, tm_batch)
+
+    # Ideally this should be pushed into the compiled train_* functions, but
+    # very strangely if we _don't_ do the reset here it results in an OOM in
+    # train_q_function. Hopefully we don't take much of a performance hit
+    # from this? (Compiling this also results in the same OOM.)
+    initial_states = self.reset_initial_states(
+        initial_states, batch.needs_reset)
+
+    # TODO: take into account delay
+    final_states = initial_states  # GC initial states as they are replaced
+    metrics = {}
+
+    (
+      policy_samples,
+      final_states['sample_policy'],
+      metrics['sample_policy'],
+    ) = self.train_sample_policy(
+        tm_frames, initial_states['sample_policy'], train)
+
+    (
+      sample_q_values,
+      final_states['q_function'],
+      metrics['q_function'],
+    ) = self.train_q_function(
+        tm_frames, initial_states['q_function'], policy_samples, train)
+
+    (
+      final_states['q_policy'],
+      metrics['q_policy'],
+    ) = self.train_q_policy(
+        tm_frames, initial_states['q_policy'], policy_samples, sample_q_values, train)
+
+    # satisfy train_q_lib._get_loss
+    metrics['total_loss'] = metrics['q_policy']['q_loss']
 
     return metrics, final_states
