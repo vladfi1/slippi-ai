e6567fa60ec6084c7d4118a002512cd0362163f3
Vlad Firoiu <vladfi2@gmail.com>
2024-10-04 15:12:17 -0400

    [twitchbot] Restart twitch stream after it dies every 2 days.

--- scripts/stream.sh
+++ scripts/stream.sh
@@ -3,14 +3,17 @@
 
 TWITCH_STREAM=rtmp://live.twitch.tv/app/$TWITCH_KEY
 
-# TODO: for some reason the twitch stream is often choppy
-# TODO: restart command every 2 days to bypass Twitch stream cap
+# Note: you need to Ctrl-C twice to exit this loop
+while true;
+do
+  ffmpeg \
+    -framerate 60 -f x11grab -i :99 \
+    -f alsa -ac 2 -i default \
+    -c:v h264_nvenc -g 120 -preset fast \
+    -b:v 3000k -maxrate 3000k -bufsize 6000k \
+    -c:a aac -ar 44100 \
+    -f flv \
+    $TWITCH_STREAM
 
-ffmpeg \
-  -framerate 60 -f x11grab -i :99 \
-  -f alsa -ac 2 -i default \
-  -c:v h264_nvenc -g 120 -preset fast \
-  -b:v 3000k -maxrate 3000k -bufsize 6000k \
-  -c:a aac -ar 44100 \
-  -f flv \
-  $TWITCH_STREAM
+  sleep 60
+done
