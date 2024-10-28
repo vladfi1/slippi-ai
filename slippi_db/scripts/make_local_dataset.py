05c2a84e3541bf55a85d9a306855d78a367ac2ba
Vlad Firoiu <vladfi2@gmail.com>
2024-10-28 11:36:54 -0400

    [dataset] Actually create meta.json in make_local_dataset.py

--- slippi_db/scripts/make_local_dataset.py
+++ slippi_db/scripts/make_local_dataset.py
@@ -1,3 +1,4 @@
+import collections
 import os
 import pickle
 import json
@@ -11,14 +12,10 @@ ROOT = flags.DEFINE_string('root', None, 'root directory', required=True)
 WINNER_ONLY = flags.DEFINE_boolean(
   'winner_only', True, 'only keep games that have a winner')
 
-SKIP_TAR = flags.DEFINE_boolean('skip_tar', False, 'Only create meta.json')
+MAKE_TAR = flags.DEFINE_boolean('tar', False, 'Create dataset tar archive')
 
 def is_valid_replay(row: dict):
   if not row.get('is_training'):
-    return False
-
-  # Mango has stated he doesn't want AI trained on his replays
-  if 'Mango' in row['raw']:
     return False
 
   return True
@@ -32,35 +29,37 @@ def main(_):
   print(f"Found {len(rows)} training replays.")
 
   # keep only games with a winner
-  # TODO: this throws away games that have a salty runback; instead we should
+  # TODO: this throws away games that have a salty runback
 
   if WINNER_ONLY.value:
     rows = [row for row in rows if row.get('winner') is not None]
     print(f"Filtered to {len(rows)} games with a winner.")
 
-  use_tar = not SKIP_TAR.value
+  make_tar = MAKE_TAR.value
 
-  if use_tar:
+  if make_tar:
     tar = tarfile.open(os.path.join(ROOT.value, 'training.tar'), 'w')
+
+  missing = collections.Counter()
 
   for row in tqdm.tqdm(rows, smoothing=0, unit='slp'):
     md5 = row['slp_md5']
     parsed_path = os.path.join(ROOT.value, 'Parsed', md5)
-    assert os.path.isfile(parsed_path), row
+    if not os.path.isfile(parsed_path):
+      missing[row['raw']] += 1
 
-    if use_tar:
+    if make_tar:
       tar.add(parsed_path, arcname='games/' + md5)
 
-  with tempfile.TemporaryDirectory() as tmpdir:
-    # write metadata
-    meta_path = os.path.join(tmpdir, 'meta.json')
-    with open(meta_path, 'w') as f:
-      json.dump(rows, f, indent=2)
+  print(f"Missing: {missing}")
 
-    if use_tar:
-      tar.add(meta_path, arcname='meta.json')
+  # write metadata
+  meta_path = os.path.join(ROOT.value, 'meta.json')
+  with open(meta_path, 'w') as f:
+    json.dump(rows, f, indent=2)
 
-  if use_tar:
+  if make_tar:
+    tar.add(meta_path, arcname='meta.json')
     tar.close()
 
 if __name__ == '__main__':
