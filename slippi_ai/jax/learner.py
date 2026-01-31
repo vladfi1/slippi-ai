614f9db793423e3dd41ceceebb6279493b43e461
Vlad Firoiu <vladfi2@gmail.com>
2026-01-30 21:13:16 -0500

    Switch to shard_map for better multidevice training.

--- slippi_ai/jax/learner.py
+++ slippi_ai/jax/learner.py
@@ -14,7 +14,9 @@ from slippi_ai.data import Frames
 from slippi_ai.jax.policies import Policy
 from slippi_ai.jax.networks import RecurrentState
 from slippi_ai.jax import value_function as vf_lib
+from slippi_ai.jax import jax_utils
 
+PS = jax.sharding.PartitionSpec
 Array = jax.Array
 
 
@@ -30,6 +32,7 @@ def swap_axes(t, axis1=0, axis2=1):
 class LearnerConfig:
   learning_rate: float = 1e-4
   reward_halflife: float = 4
+  use_shard_map: bool = True
 
 # TODO: move to jax_utils
 P = tp.ParamSpec('P')
@@ -50,6 +53,36 @@ def jit_method(
 
   return nnx.cached_partial(jitted, method.__self__)
 
+def _sharding_callback(sharding: tp.Optional[jax.sharding.Sharding]):
+  print(sharding)
+  import ipdb; ipdb.set_trace()
+
+Metrics = dict[str, tp.Any]
+
+def _policy_loss_fn(
+    policy: Policy, data: tuple[Frames, RecurrentState],
+# ) -> tp.Tuple[Metrics, RecurrentState]:
+) -> tuple[jax_utils.Loss, tp.Tuple[Metrics, RecurrentState]]:
+  frames, initial_states = data
+  tm_frames: Frames = jax.tree.map(swap_axes, frames)
+  loss, tm_metrics, final_states = policy.imitation_loss(tm_frames, initial_states)
+  # metrics = jax.tree.map(lambda t: jnp.mean(t, axis=0), metrics)
+  bm_metrics = jax.tree.map(swap_axes, tm_metrics)
+  return loss, (bm_metrics, final_states)
+
+def _value_loss_fn(
+    value_function: vf_lib.ValueFunction,
+    data: tuple[Frames, RecurrentState],
+    discount: float,
+# ) -> tp.Tuple[Metrics, RecurrentState]:
+) -> tuple[jax_utils.Loss, tp.Tuple[Metrics, RecurrentState]]:
+  frames, initial_states = data
+  tm_frames: Frames = jax.tree.map(swap_axes, frames)
+  value_outputs, final_states = value_function.loss(
+      tm_frames, initial_states, discount)
+  loss = jnp.mean(value_outputs.loss)
+  bm_metrics = jax.tree.map(swap_axes, value_outputs.metrics)
+  return loss, (bm_metrics, final_states)
 
 class Learner(nnx.Module):
 
@@ -59,12 +92,15 @@ class Learner(nnx.Module):
       learning_rate: float,
       reward_halflife: float,
       value_function: vf_lib.ValueFunction,
+      mesh: jax.sharding.Mesh,
       compile: bool = True,
+      use_shard_map: bool = True,
   ):
     self.policy = policy
     self.value_function = value_function
     self.discount = 0.5 ** (1 / (reward_halflife * 60))
     self.compile = compile
+    self.use_shard_map = use_shard_map
 
     # Create optimizers using optax
     self.policy_optimizer = nnx.Optimizer(
@@ -79,6 +115,37 @@ class Learner(nnx.Module):
     self.jit_step_value_function = jit_method(
         self._step_value_function, static_argnames=('train',))
 
+    jax_utils.replicate_module(self, mesh)
+
+    # Train and run functions using shard_map. Empirically these have better
+    # performance for the frame_tx network than the above "jit_step" methods,
+    # which let XLA handle sharding automatically.
+    self.sharded_train_policy = jax_utils.data_parallel_train(
+        module=self.policy,
+        optimizer=self.policy_optimizer,
+        loss_fn=_policy_loss_fn,
+        mesh=mesh,
+    )
+
+    self.sharded_run_policy = jax_utils.shard_map_loss_fn(
+        module=self.policy,
+        loss_fn=_policy_loss_fn,
+        mesh=mesh,
+    )
+
+    self.sharded_train_value_function = jax_utils.data_parallel_train(
+        module=self.value_function,
+        optimizer=self.value_optimizer,
+        loss_fn=_value_loss_fn,
+        mesh=mesh,
+    )
+
+    self.sharded_run_value_function = jax_utils.shard_map_loss_fn(
+        module=self.value_function,
+        loss_fn=_value_loss_fn,
+        mesh=mesh,
+    )
+
   def initial_state(self, batch_size: int, rngs: nnx.Rngs) -> RecurrentState:
     return (
         self.policy.initial_state(batch_size, rngs),
@@ -90,27 +157,30 @@ class Learner(nnx.Module):
       bm_frames: Frames,
       initial_states: RecurrentState,
       train: bool = True,
-  ) -> tuple[Array, dict, RecurrentState]:
+  ) -> tuple[Array, Metrics, RecurrentState]:
     """Single training/eval step for policy only."""
     # Switch axes to time-major
     tm_frames: Frames = jax.tree.map(swap_axes, bm_frames)
 
     # Define loss function that takes policy as argument for gradient computation
-    def policy_loss_fn(policy: Policy):
-      policy_loss, policy_final_states, policy_metrics = policy.imitation_loss(
+    def loss_fn(policy: Policy):
+      loss, metrics, final_states = policy.imitation_loss(
           tm_frames, initial_states)
-      return policy_loss, (policy_metrics, policy_final_states)
+      return loss, (metrics, final_states)
 
     if train:
       # Compute gradients for policy using value_and_grad to also get loss
-      (loss, (metrics, final_states)), policy_grads = nnx.value_and_grad(
-          policy_loss_fn, has_aux=True)(self.policy)
+      (loss, (metrics, final_states)), grads = nnx.value_and_grad(
+          loss_fn, has_aux=True)(self.policy)
+
+      # jax.debug.inspect_array_sharding(final_states, callback=_sharding_callback)
+      # jax.debug.inspect_array_sharding(grads, callback=_sharding_callback)
 
       # Apply gradients
-      self.policy_optimizer.update(self.policy, policy_grads)
+      self.policy_optimizer.update(self.policy, grads)
     else:
       # Just compute forward pass without gradients
-      loss, (metrics, final_states) = policy_loss_fn(self.policy)
+      loss, (metrics, final_states) = loss_fn(self.policy)
 
 
     return loss, metrics, final_states
@@ -120,7 +190,7 @@ class Learner(nnx.Module):
       bm_frames: Frames,
       initial_states: RecurrentState,
       train: bool = True,
-  ) -> tuple[Array, dict, RecurrentState]:
+  ) -> tuple[Array, Metrics, RecurrentState]:
     """Single training/eval step for value function only."""
     # Switch axes to time-major
     tm_frames: Frames = jax.tree.map(swap_axes, bm_frames)
@@ -157,24 +227,34 @@ class Learner(nnx.Module):
   ) -> tuple[dict, RecurrentState]:
     """Single training/eval step."""
     compile = self.compile if compile is None else compile
-    if compile:
-      step_policy_fn = self.jit_step_policy
-      step_value_fn = self.jit_step_value_function
-    else:
-      step_policy_fn = self._step_policy
-      step_value_fn = self._step_value_function
 
     policy_initial_states, value_initial_states = initial_states
 
-    policy_loss, policy_metrics, policy_final_states = step_policy_fn(
-        bm_frames, policy_initial_states, train=train)
-    value_loss, value_metrics, value_final_states = step_value_fn(
-        bm_frames, value_initial_states, train=train)
+    if compile and self.use_shard_map:
+      if train:
+        policy_metrics, policy_final_states = self.sharded_train_policy(
+            (bm_frames, policy_initial_states))
+        value_metrics, value_final_states = self.sharded_train_value_function(
+            (bm_frames, value_initial_states), discount=self.discount)
+      else:
+        policy_metrics, policy_final_states = self.sharded_run_policy(
+            (bm_frames, policy_initial_states))
+        value_metrics, value_final_states = self.sharded_run_value_function(
+            (bm_frames, value_initial_states), discount=self.discount)
+    else:
+      if compile:
+        step_policy_fn = self.jit_step_policy
+        step_value_fn = self.jit_step_value_function
+      else:
+        step_policy_fn = self._step_policy
+        step_value_fn = self._step_value_function
 
-    total_loss = policy_loss + value_loss
+      _, policy_metrics, policy_final_states = step_policy_fn(
+          bm_frames, policy_initial_states, train=train)
+      _, value_metrics, value_final_states = step_value_fn(
+          bm_frames, value_initial_states, train=train)
 
     metrics = dict(
-        total_loss=total_loss,
         policy=policy_metrics,
         value=value_metrics,
     )
