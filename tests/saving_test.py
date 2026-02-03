5cd0a195346e7737efd3dec890f48cd1180d3d94
Vlad Firoiu <vladfi2@gmail.com>
2026-02-02 21:46:28 -0500

    Test replay parsing on github.

--- tests/saving_test.py
+++ tests/saving_test.py
@@ -5,4 +5,4 @@ from slippi_ai import saving, paths
 class SavingTest(unittest.TestCase):
 
   def test_load_demo_checkpoint(self):
-    saving.load_policy_from_disk(paths.DEMO_CHECKPOINT)
+    saving.load_policy_from_disk(str(paths.DEMO_CHECKPOINT))
