ccdff1b0f871ad967a6db1b498107b2ae81c18de
Vlad Firoiu <vladfi2@gmail.com>
2026-02-06 09:31:33 -0500

    Prevent imitation test from hanging.

--- tests/training_test.sh
+++ tests/training_test.sh
@@ -14,7 +14,7 @@ python scripts/train.py \
   --config.runtime.log_interval=4 \
   --config.runtime.max_runtime=10 \
   --config.runtime.num_evals_per_epoch=1 \
-  --config.runtime.num_eval_epochs=1 \
+  --config.runtime.max_eval_steps=2 \
   --config.network.name=tx_like \
   --config.network.tx_like.hidden_size=2 \
   --config.value_function.separate_network_config=False \
