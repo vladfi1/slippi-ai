480a073950166aa5e57c27ae48e6f2e6628dafc0
Vlad Firoiu <vladfi2@gmail.com>
2026-02-07 09:33:24 -0500

    Get RL running post-refactor.

--- tests/no_tf_import.py
+++ tests/no_tf_import.py
@@ -1,7 +1,7 @@
 """Tests that tensorflow is not imported when importing neutral or jax code."""
 
 import sys
-from slippi_ai import envs, saving, eval_lib, data, utils
+from slippi_ai import envs, saving, eval_lib, data, utils, evaluators
 from slippi_ai.jax import *
 
 if __name__ == '__main__':
