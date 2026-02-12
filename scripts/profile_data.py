06043be0fcc2dece052118c5fd6e16d3e2030e85
Vlad Firoiu <vladfi2@gmail.com>
2026-02-12 14:12:41 -0500

    Update profile_data.py

--- scripts/profile_data.py
+++ scripts/profile_data.py
@@ -1,36 +1,47 @@
 import dataclasses
 import time
+import logging
 
 from absl import app, flags
+import fancyflags as ff
 
-from slippi_ai import data, paths
+from slippi_ai import data as data_lib, paths, flag_utils
 from slippi_ai import observations
 
-DATA_DIR = flags.DEFINE_string('data_dir', str(paths.TOY_DATA_DIR), 'path to data directory')
-META_PATH = flags.DEFINE_string('meta_path', str(paths.TOY_META_PATH), 'path to meta file')
+_field = lambda f: dataclasses.field(default_factory=f)
 
-BATCH_SIZE = flags.DEFINE_integer('batch_size', 32, 'batch size')
-UNROLL_LENGTH = flags.DEFINE_integer('unroll_length', 32, 'unroll length')
+@dataclasses.dataclass
+class Config:
+  dataset: data_lib.DatasetConfig = _field(data_lib.DatasetConfig)
+  data: data_lib.DataConfig = _field(data_lib.DataConfig)
+  observation: observations.ObservationConfig = _field(observations.ObservationConfig)
+
+
+DEFAULT_CONFIG = Config()
+DEFAULT_CONFIG.dataset.dataset_path = str(paths.TOY_DATASET)
+
+CONFIG = ff.DEFINE_dict('config', **flag_utils.get_flags_from_default(DEFAULT_CONFIG))
+
 RUNTIME = flags.DEFINE_integer('runtime', 5, 'runtime in seconds')
 
+
+
 def main(_):
-  dataset_config = data.DatasetConfig(
-      data_dir=DATA_DIR.value,
-      meta_path=META_PATH.value,
-  )
-  train, _ = data.train_test_split(dataset_config)
+  logging.getLogger("dropbox").setLevel(logging.WARNING)
 
-  data_config = data.DataConfig(
-      batch_size=BATCH_SIZE.value,
-      unroll_length=UNROLL_LENGTH.value,
-      num_workers=0,
-  )
+  config = flag_utils.dataclass_from_dict(Config, CONFIG.value)
+  train, _ = data_lib.train_test_split(config.dataset)
 
-  source = data.make_source(
+  source = data_lib.make_source(
       replays=train,
-      observation_config=observations.ObservationConfig(),
-      **dataclasses.asdict(data_config),
+      observation_config=config.observation,
+      **dataclasses.asdict(config.data),
   )
+
+  print('Warming up...')
+  start = time.perf_counter()
+  next(source)  # warmup
+  print(f'Warmup took {time.perf_counter() - start:.2f} seconds')
 
   start = time.perf_counter()
   batches = 0
@@ -39,11 +50,15 @@ def main(_):
     batches += 1
   run_time = time.perf_counter() - start
   bps = batches / run_time
-  sbp = 1 / bps
+  spb = 1 / bps
 
-  frames_per_batch = data_config.unroll_length * data_config.batch_size
+  frames_per_batch = config.data.unroll_length * config.data.batch_size
   fps = frames_per_batch * bps
-  print(f'bps={bps:.2f} fps={fps:.2f} sbp={sbp:.3f}')
+
+  frames_per_minute = 60 * 60
+  mps = fps / frames_per_minute
+
+  print(f'bps={bps:.2f} spb={spb:.3f} fps={fps:.0f} mps={mps:.1f}')
 
 if __name__ == '__main__':
   app.run(main)
