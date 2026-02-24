557e513caafb467a676dc6ee153d14e0f17f2579
Vlad Firoiu <vladfi2@gmail.com>
2026-02-24 17:19:24 -0500

    Relax unroll_agent relative tolerance.

--- slippi_ai/unroll_agent.py
+++ slippi_ai/unroll_agent.py
@@ -60,7 +60,7 @@ def unroll(
 
 def check_arrays_with_path(path: tuple, xs: np.ndarray, ys: np.ndarray):
   try:
-    np.testing.assert_allclose(xs, ys, atol=1e-5, rtol=1e-5)
+    np.testing.assert_allclose(xs, ys, atol=1e-5, rtol=1e-3)
   except AssertionError as e:
     raise ValueError(f'Output mismatch at {path}: {e}') from e
 
