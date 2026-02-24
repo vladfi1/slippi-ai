6c280dbb045b6e835c1d441bd2e73a55b1ade48c
Vlad Firoiu <vladfi2@gmail.com>
2026-02-24 10:09:10 -0500

    Rename q_learner/train_q_lib as combined.

--- slippi_ai/jax/q/tests/train_combined.py
+++ slippi_ai/jax/q/tests/train_combined.py
@@ -8,7 +8,7 @@ import fancyflags as ff
 from slippi_ai import paths, flag_utils
 from slippi_ai import data as data_lib
 from slippi_ai.jax import networks
-from slippi_ai.jax.q import train_q_lib, q_learner
+from slippi_ai.jax.q import combined_learner, train_combined
 
 network_config = networks.default_config()
 network_config['name'] = 'tx_like'
@@ -20,7 +20,7 @@ network_config['tx_like'].update(
     activation='gelu',
 )
 
-DEFAULT_CONFIG = train_q_lib.Config(
+DEFAULT_CONFIG = train_combined.Config(
     dataset=data_lib.DatasetConfig(
         data_dir=str(paths.TOY_DATA_DIR),
         meta_path=str(paths.TOY_META_PATH),
@@ -32,14 +32,14 @@ DEFAULT_CONFIG = train_q_lib.Config(
         batch_size=2,
         unroll_length=5,
     ),
-    runtime=train_q_lib.RuntimeConfig(
+    runtime=train_combined.RuntimeConfig(
         log_interval=4,
         max_runtime=10,
         num_evals_per_epoch=2,
         num_eval_epochs=0.1,
     ),
     network=network_config,
-    q_function=train_q_lib.QFunctionConfig(
+    q_function=train_combined.QFunctionConfig(
         network=network_config,
     ),
 )
@@ -62,8 +62,8 @@ if __name__ == '__main__':
     wandb.init(mode='offline')  # avoid network calls during tests
 
     config = flag_utils.dataclass_from_dict(
-        train_q_lib.Config, CONFIG.value)
+        train_combined.Config, CONFIG.value)
 
-    train_q_lib.train(config)
+    train_combined.train(config)
 
   app.run(main)
