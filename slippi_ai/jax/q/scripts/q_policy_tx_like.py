774b5a971447ff87b2a33ef28f611d60856aaf11
Vlad Firoiu <vladfi2@gmail.com>
2026-02-25 14:59:50 -0500

    Train only q_policy, relying on pre-trained q_function.

--- slippi_ai/jax/q/scripts/q_policy_tx_like.py
+++ slippi_ai/jax/q/scripts/q_policy_tx_like.py
@@ -10,16 +10,14 @@ import fancyflags as ff
 
 import melee
 from slippi_ai import flag_utils, paths
-from slippi_ai.jax import embed
-from slippi_ai.jax.q import train_combined
+from slippi_ai.jax import saving, train_lib
+from slippi_ai.jax.q import train_q_policy
 
 NET_NAME = 'tx_like'
 
 def default_config():
-  config = train_combined.Config()
+  config = train_q_policy.Config()
 
-  config.max_names = 128
-  config.policy.delay = 21
   config.data.batch_size = 512
   config.data.unroll_length = 80
   config.data.damage_ratio = 0.01
@@ -27,25 +25,6 @@ def default_config():
   config.data.balance_characters = True
   config.learner.learning_rate = 1e-4
   config.learner.reward_halflife = 8
-  config.embed.controller.type = embed.ControllerType.CUSTOM_V1.value
-  config.embed.player.with_nana = True
-  # MLP Items embed not yet implemented for JAX
-  config.embed.items.type = embed.ItemsType.FLAT
-  # config.embed.items.type = embed.ItemsType.MLP
-  # config.embed.items.mlp_sizes = (128, 32)
-  config.embed.with_fod = True
-  config.embed.with_randall = True
-
-  net_config =  config.q_function.network
-  net_config['name'] = NET_NAME
-  net_config[NET_NAME].update(
-      num_layers=2,
-      hidden_size=512,
-  )
-  net_config['embed']['name'] = 'enhanced'
-  net_config['embed']['enhanced'].update(
-      use_controller_rnn=True,
-  )
 
   config.dataset.mirror = True
   config.dataset.allowed_opponents='all'
@@ -60,24 +39,6 @@ def default_config():
 if __name__ == '__main__':
   # https://github.com/python/cpython/issues/87115
   __spec__ = None
-
-  NET = ff.DEFINE_dict(
-      'net',
-      name=ff.String(NET_NAME),
-      hidden_size=ff.Integer(512),
-      ffw_multiplier=ff.Integer(2),
-      recurrent_layer=ff.String('lstm'),
-      # Omit num_layers to allow different layers for policy and value nets
-  )
-  EMBED = ff.DEFINE_dict(
-      'embed',
-      name=ff.String('enhanced'),
-      simple=dict(),
-      enhanced=dict(
-          rnn_cell=ff.String('lstm'),
-          use_controller_rnn=ff.Boolean(False),
-      ),
-  )
 
   TOY_DATA = flags.DEFINE_bool('toy_data', False, 'Use toy data for quick testing')
 
@@ -100,11 +61,14 @@ if __name__ == '__main__':
   )
 
   def main(_):
-    config = flag_utils.dataclass_from_dict(train_combined.Config, CONFIG.value)
+    config = flag_utils.dataclass_from_dict(train_q_policy.Config, CONFIG.value)
     config.runtime.max_runtime = int(NUM_DAYS.value * 24 * 60 * 60)
 
-    net_config = dict(NET.value)
-    net = net_config.pop('name')
+    assert config.initialize_policies_from is not None
+    imitation_state = saving.load_state_from_disk(config.initialize_policies_from)
+    imitation_config = flag_utils.dataclass_from_dict(
+        train_lib.Config,
+        saving.upgrade_config(imitation_state['config']))
 
     if TOY_DATA.value:
       config.dataset.data_dir = str(paths.TOY_DATA_DIR)
@@ -119,27 +83,15 @@ if __name__ == '__main__':
       char = CHAR.value
 
       if config.tag is None:
-        n = config.q_function.network[net]['num_layers']
-        h = net_config['hidden_size']
-        config.tag = f"{char}_{net}_{n}x{h}"
+        network = imitation_config.network
+        assert network['name'] == NET_NAME, f"Expected network name {NET_NAME} but got {network['name']}"
+        d = imitation_config.policy.delay
+        n = network[NET_NAME]['num_layers']
+        h = network[NET_NAME]['hidden_size']
+        fs = imitation_config.observation.frame_skip.skip
+        config.tag = f"{char}_d{d}_{NET_NAME}_{n}x{h}_fs{fs}"
 
     config.dataset.allowed_characters = char
-
-    embed_config = dict(EMBED.value)
-    embed_name = embed_config['name']
-    embed_config['enhanced']['hidden_size'] = net_config['hidden_size'] // 4
-    embed_config['enhanced']['use_self_nana'] = char in ['popo', 'all']
-
-    def update_embed_config(config: dict):
-      config['name'] = embed_name
-      config[embed_name].update(embed_config[embed_name])
-
-    def update_network_config(config: dict):
-      config['name'] = net
-      config[net].update(net_config)
-      update_embed_config(config['embed'])
-
-    update_network_config(config.q_function.network)
 
     wandb_kwargs = dict(WANDB.value)
     if wandb_kwargs['name'] is None:
@@ -151,6 +103,6 @@ if __name__ == '__main__':
         config=dataclasses.asdict(config),
         **wandb_kwargs,
     )
-    train_combined.train(config)
+    train_q_policy.train(config)
 
   app.run(main)
