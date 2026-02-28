2fecd56f1aeb3c1d4583c0a8aef7b10b641c29cc
Vlad Firoiu <vladfi2@gmail.com>
2026-02-28 18:34:39 -0500

    Default emulation speed to 0 for DolphinConfig.

--- slippi_ai/dolphin.py
+++ slippi_ai/dolphin.py
@@ -305,7 +305,7 @@ class DolphinConfig:
   disable_audio: bool = False  # Disable dolphin audio.
   audio_backend: str = ''  # Audio backend to use.
   headless: bool = True  # Headless configuration: exi + ffw, no graphics or audio.
-  emulation_speed: float = 1.0  # Set to 0 for unlimited speed. Mainline only.
+  emulation_speed: float = 0.0  # Set to 0 for unlimited speed. Mainline only.
   infinite_time: bool = True  # Infinite time no stocks.
   log_level: int = 3  # WARN; 0 to disable
   log_types: list[str] = dataclasses.field(default_factory=['SLIPPI'].copy)
