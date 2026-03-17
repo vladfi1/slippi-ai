e4cae68a30cbf8b20fbe6f60deb9e2135a360bde
Vlad Firoiu <vladfi2@gmail.com>
2026-03-17 08:24:59 -0400

    profile_data with yappi

--- scripts/profile_data.py
+++ scripts/profile_data.py
@@ -1,6 +1,7 @@
 import dataclasses
 import time
 import logging
+import yappi
 
 from absl import app, flags
 import fancyflags as ff
@@ -24,7 +25,7 @@ CONFIG = ff.DEFINE_dict('config', **flag_utils.get_flags_from_default(DEFAULT_CO
 
 RUNTIME = flags.DEFINE_integer('runtime', 5, 'runtime in seconds')
 
-
+YAPPI = flags.DEFINE_bool('yappi', False, 'Whether to profile with yappi.')
 
 def main(_):
   logging.getLogger("dropbox").setLevel(logging.WARNING)
@@ -38,28 +39,39 @@ def main(_):
       max_names=16,
   )
   train = sources.train
+  try:
+    print('Warming up...')
+    start = time.perf_counter()
+    next(train)  # warmup
+    print(f'Warmup took {time.perf_counter() - start:.2f} seconds')
 
-  print('Warming up...')
-  start = time.perf_counter()
-  next(train)  # warmup
-  print(f'Warmup took {time.perf_counter() - start:.2f} seconds')
+    if YAPPI.value:
+      yappi.start()
 
-  start = time.perf_counter()
-  batches = 0
-  while time.perf_counter() - start < RUNTIME.value:
-    next(train)
-    batches += 1
-  run_time = time.perf_counter() - start
-  bps = batches / run_time
-  spb = 1 / bps
+    start = time.perf_counter()
+    batches = 0
+    while time.perf_counter() - start < RUNTIME.value:
+      next(train)
+      batches += 1
+    run_time = time.perf_counter() - start
+    bps = batches / run_time
+    spb = 1 / bps
 
-  frames_per_batch = config.data.unroll_length * config.data.batch_size
-  fps = frames_per_batch * bps
+    frames_per_batch = config.data.unroll_length * config.data.batch_size
+    fps = frames_per_batch * bps
 
-  frames_per_minute = 60 * 60
-  mps = fps / frames_per_minute
+    frames_per_minute = 60 * 60
+    mps = fps / frames_per_minute
 
-  print(f'batches={batches} bps={bps:.2f} spb={spb:.3f} fps={fps:.0f} mps={mps:.1f}')
+    print(f'batches={batches} bps={bps:.2f} spb={spb:.3f} fps={fps:.0f} mps={mps:.1f}')
+  finally:
+    train.shutdown()
+    sources.test.shutdown()
+    if YAPPI.value:
+      yappi.stop()
+
+  if YAPPI.value:
+    yappi.get_func_stats().print_all()
 
 if __name__ == '__main__':
   app.run(main)
