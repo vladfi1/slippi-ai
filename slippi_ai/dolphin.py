1e641221025ff6893b409f584b072ff29624dfcc
Vlad Firoiu <vladfi2@gmail.com>
2026-04-25 13:25:58 -0400

    Disable instant_match_restart as it results in dolphin crashing more often.

--- slippi_ai/dolphin.py
+++ slippi_ai/dolphin.py
@@ -308,7 +308,7 @@ class DolphinConfig:
   headless: bool = True  # Headless configuration: exi + ffw, no graphics or audio.
   emulation_speed: float = 0.0  # Set to 0 for unlimited speed. Mainline only.
   infinite_time: bool = True  # Infinite time no stocks.
-  instant_match_restart: bool = True  # Skip menuing between games, start new game on random stage.
+  instant_match_restart: bool = False  # Skip menuing between games, start new game on random stage.
   log_level: int = 3  # WARN; 0 to disable
   log_types: list[str] = dataclasses.field(default_factory=['SLIPPI'].copy)
   dump: DumpConfig = _field(DumpConfig)  # For framedumping.
