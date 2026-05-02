a21e67a0fd2ae7c68ac78e06bbb6178a75626f4c
Vlad Firoiu <vladfi2@gmail.com>
2026-05-02 18:15:20 -0400

    Initiate transfer back to host for all frameskipped actions in jax agent.

--- slippi_ai/jax/agents.py
+++ slippi_ai/jax/agents.py
@@ -175,14 +175,13 @@ class BasicAgent(agents.BasicAgent[ControllerType, policies.RecurrentState]):
       # Use donate_argnums?
       self._prev_controller = [so.controller_state for so in sample_outputs]
 
+      sample_outputs = jax.copy_to_host_async(sample_outputs)
       self._sample_outputs.extend(sample_outputs)
 
       self._needs_reset[:] = False
 
-    sample_outputs = self._sample_outputs.popleft()
-
     # Convert to numpy?
-    return jax.copy_to_host_async(sample_outputs)
+    return self._sample_outputs.popleft()
 
   def multi_step(
       self,
