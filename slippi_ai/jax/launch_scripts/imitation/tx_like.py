fca156e5ad32296ef45e04c7524135639e822a11
Vlad Firoiu <vladfi2@gmail.com>
2026-05-19 13:20:14 -0400

    [imitation] Fix auto-generated experiment tag.

--- slippi_ai/jax/launch_scripts/imitation/tx_like.py
+++ slippi_ai/jax/launch_scripts/imitation/tx_like.py
@@ -160,10 +160,13 @@ if __name__ == '__main__':
         rfs = config.policy.frame_skip
         rfs = f'rfs{rfs}'
 
-        chd = config.controller_head['autoregressive']['component_depth']
-        chs = config.controller_head['autoregressive']['residual_size']
+        ch = config.controller_head['autoregressive']['component']
+        assert ch['name'] == 'tx_like'
+        ch = ch['tx_like']
+        chn = ch['num_layers']
+        chs = ch['hidden_size']
 
-        config.tag = f"{char}_d{delay}{op}_{n}x{h}_ch{chd}x{chs}_{rfs}"
+        config.tag = f"{char}_d{delay}{op}_{n}x{h}_ch{chn}x{chs}_{rfs}"
 
     config.dataset.allowed_characters = char
 
