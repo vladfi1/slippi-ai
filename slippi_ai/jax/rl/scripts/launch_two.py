8f8b6f3036ab7f47c9902abd51eb4614aa39e5df
Vlad Firoiu <vladfi2@gmail.com>
2026-08-07 17:44:01 -0400

    [train_two] Modernize launch script: optimal dtypes, sim envs, perf flag.

--- slippi_ai/jax/rl/scripts/launch_two.py
+++ slippi_ai/jax/rl/scripts/launch_two.py
@@ -10,6 +10,8 @@ if __name__ == '__main__':
   import logging
   import os
 
+  os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '1'
+
   from absl import app, flags
   import fancyflags as ff
   import wandb
@@ -17,22 +19,7 @@ if __name__ == '__main__':
   from slippi_ai import flag_utils
   from slippi_ai.jax import saving, train_lib
   from slippi_ai.jax.rl import train_two_lib
-
-  PP="Platinum Player"
-  DP="Diamond Player"
-  MP="Master Player"
-
-  D1=21
-
-  P1='marth'
-  N1=['Zain', 'Kodorin', MP]
-  M1=f'{P1}_d{D1}_imitation_3x768'
-
-  D2=18
-
-  P2='luigi'
-  N2=['Siddward', 'JahRidin', MP]
-  M2=f'{P2}_d{D2}_tx_like_3x512'
+  from slippi_ai.jax.agents import DType
 
   PGW=3
 
@@ -43,25 +30,24 @@ if __name__ == '__main__':
   CONFIG.dolphin.path=os.environ.get('MAINLINE_EXI_AI')
   CONFIG.dolphin.iso=os.environ.get('ISO_PATH')
   CONFIG.dolphin.console_timeout=60
-  CONFIG.dolphin.infinite_time=True
+  CONFIG.dolphin.infinite_time=False  # regularly randomize stages
   CONFIG.dolphin.emulation_speed=0
   CONFIG.learner.learning_rate=3e-5
   CONFIG.learner.value_cost=1
   CONFIG.learner.policy_gradient_weight=PGW
   CONFIG.learner.reward_halflife=4
   CONFIG.learner.ppo.num_epochs=2
-  CONFIG.learner.ppo.num_batches=16
+  CONFIG.learner.ppo.num_batches=1
   CONFIG.learner.ppo.beta=3e-1
   CONFIG.learner.ppo.epsilon=1e-2
   CONFIG.p1.batch_steps=4
   CONFIG.p2.batch_steps=4
-  CONFIG.p1.teacher=f'pickled_models/jax/{M1}'
-  CONFIG.p2.teacher=f'pickled_models/jax/{M2}'
-  CONFIG.p1.name=N1
-  CONFIG.p2.name=N2
   CONFIG.actor.rollout_length=80
-  CONFIG.actor.num_envs=200
-  CONFIG.actor.inner_batch_size=8
+  CONFIG.actor.use_sim_envs=True
+  CONFIG.actor.num_envs=2048
+  CONFIG.learner.microbatch_size=512
+  CONFIG.learner.value_mbs=1024
+  CONFIG.actor.inner_batch_size=-1
   CONFIG.actor.async_envs=True
   CONFIG.actor.num_env_steps=4
   CONFIG.actor.gpu_inference=True
@@ -69,6 +55,13 @@ if __name__ == '__main__':
   CONFIG.runtime.reset_every_n_steps=None
   CONFIG.learner.optimizer_burnin_epochs=0
   CONFIG.learner.value_burnin_epochs=0
+
+  # Optimal dtypes
+  CONFIG.p1.jax.dtype = DType.FP16
+  CONFIG.p2.jax.dtype = DType.FP16
+  CONFIG.learner.teacher_dtype = DType.FP16
+  CONFIG.learner.value_dtype = DType.BF16
+  CONFIG.learner.policy_dtype = DType.FP16
 
   CONFIG_FLAG = ff.DEFINE_dict(
       'config',
@@ -86,6 +79,8 @@ if __name__ == '__main__':
   )
 
   KLW = flags.DEFINE_float('kl_weight', 1e-2, 'weight for KL teacher losses')
+
+  PERF = flags.DEFINE_bool('perf', False, 'Run to measure performance')
 
   def get_imitation_config(config_path: str) -> train_lib.Config:
     imitation_state = saving.load_state_from_disk(config_path)
@@ -125,6 +120,15 @@ if __name__ == '__main__':
       config.runtime.tag = f"{c1}_d{d1}_vs_{c2}_d{d2}_kl_{KLW.value:.0e}"
 
     wandb_kwargs = dict(WANDB_FLAG.value)
+
+    if PERF.value:
+      config.runtime.save_interval = -1
+      config.runtime.log_interval = 20
+      config.learner.optimizer_burnin_epochs = 0
+      config.learner.value_burnin_epochs = 0
+      # config.actor.use_sim_envs = False
+      # config.actor.use_fake_envs = True
+      wandb_kwargs['mode'] = 'disabled'
 
     if wandb_kwargs['name'] is None:
       wandb_kwargs['name'] = config.runtime.tag
